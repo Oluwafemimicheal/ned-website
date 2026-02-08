095c2216b7f16cb1a506666ce44d23315fee16b2
oluwafemi michael <femi4410@gmail.com>
2026-02-08 11:57:15 +0100

    Working on the Chat Component

--- src/components/ui/Chat.jsx
+++ src/components/ui/Chat.jsx
@@ -2,9 +2,9 @@ import { IoIosChatbubbles } from "react-icons/io";
 
 const Chat = () => {
   return (
-    <div className='fixed h-screen  bg-amber-200 right-4 z-99'>
-      <div className='absolute bottom-5 right-1 flex justify-center items-center w-16 h-16 bg-white rounded-full shadow-md cursor-pointer'>
-        <IoIosChatbubbles size={40} color="green" />
+    <div className='fixed h-screen  bg-amber-200 right-4 z-99 transition'>
+      <div className='absolute bottom-5 right-1 flex justify-center items-center w-12 h-12 opacity-50 hover:opacity-100 hover:transform-[translateY(-10px)] bg-black rounded-full shadow-md cursor-pointer'>
+        <IoIosChatbubbles size={30} color="white" />
       </div>
     </div>
   )
